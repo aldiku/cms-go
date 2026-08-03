846f671bf1a7c570c4ccd239e822d52b02fbc1cf
aldiku <ncs.aldi@gmail.com>
2026-08-03 19:16:01 +0700

    feat: implement graceful shutdown and signal handling for server

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"cms-go/internal/server"
 
 	"github.com/joho/godotenv"
@@ -9,5 +17,20 @@ import (
 func main() {
 	godotenv.Load()
 	srv := server.New()
-	srv.Logger.Fatal(srv.Start(":8080"))
+
+	go func() {
+		if err := srv.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			srv.Logger.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		srv.Logger.Fatal(err)
+	}
 }
